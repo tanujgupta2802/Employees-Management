784b04690fc417e6e0b9e906f69499acf8c03f11
tanujgupta2802 <tanujg2802@gmail.com>
2025-06-29 22:42:28 +0530

    Adding main header

---
+++ src/Smaller Components/MainHeading.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const MainHeading = () => {
+  return (
+    <>
+      <div>
+        <h1 className="text-center text-[20px]  sm:text-3xl pt-12 font-bold text-white">
+          Employee Task Management System
+        </h1>
+      </div>
+    </>
+  );
+};
+
+export default MainHeading;
--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import MainHeading from "../../Smaller Components/MainHeading";
 
 const Login = ({ handleLogin }) => {
   const [email, setEmail] = useState("");
@@ -24,6 +25,7 @@ const Login = ({ handleLogin }) => {
   return (
     <>
       <section className="min-h-screen bg-gray-50 dark:bg-gray-900">
+        <MainHeading />
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
           <h2 className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
             Login Form
